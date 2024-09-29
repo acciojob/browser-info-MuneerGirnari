69dabc3ceeaeb0960006ea218dc1c5731082e7aa
Muneer Girnari <muneeragirnari@gmail.com>
2024-09-30 00:38:05 +0530

    html and js updated

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let obj = {
+    browserName : navigator.appName,
+    version : navigator.appVersion
+}
+
+let string = `You are using + ${obj.browserName} + version + ${obj.version}`;
+
+let container = document.getElementById("browser-info");
+
+container.append(string);
